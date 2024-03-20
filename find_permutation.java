4db95d65edee214bcd8744863e130b6df37625b4
as4anmol <148984329+as4anmol@users.noreply.github.com>
2024-03-20 23:13:28 +0530

    Add files via upload

---
+++ find_permutation.java
@@ -0,0 +1,30 @@
+//(Q) Find permutation.. find & print all permutation of a givrn string "abc".
+
+public class find_permutation {
+    //(Q) Find permutation.. find & print all permutation of a givrn string "abc".
+
+public static void findPermutation(String str, String ans) {
+    //base case
+    if(str.length()==0){
+        System.out.println(ans);
+        return;
+    }
+    //recursion
+    for(int i=0;i<str.length();i++){
+        char curr=str.charAt(i);
+        String NewStr=str.substring(0,i)+str.substring(i+1);
+        findPermutation(NewStr, ans+curr);
+        
+    }
+        
+    }
+    
+    public static void main(String[] args) {
+        String str="abc";
+        findPermutation(str,"");
+    }
+        
+    }
+    
+    
+
